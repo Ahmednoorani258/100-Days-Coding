ef584092c646be9b9e339b7aaca7f0ef5d8a7827
Muhammad Ahmed <ahmednoorani258@gmail.com>
2024-03-19 09:18:29 -0700

    update

---
+++ 18 day/q52.ts
@@ -0,0 +1,16 @@
+
+// Question 52: Make a Smartphone Object: Create a simple way to keep track of a smartphone's details. Include its brand, model, and other key features like how much storage it has, the size of its screen, and how long its battery lasts.
+
+// Explain & TIP: Just like a box can contain smaller boxes, objects can contain other objects. This helps organize related information neatly.
+
+let smartphone = {
+    brand: "realme",
+    model: "c51",
+    feautures:{
+        camera: "50mxp",
+        battery: "5000mAh",
+        color: "seagreen"
+    }
+}
+
+console.log(smartphone);
