e2d3d2140dfb9890c5f7052f1213636ada2db373
Muhammad Ahmed <ahmednoorani258@gmail.com>
2024-04-02 15:52:25 -0700

    update

--- 33 day/q99.js
+++ 33 day/q99.js
@@ -1,17 +1,14 @@
-"use strict";
 // Question 99: Generate a date object representing your next birthday and log it to the console.
-Object.defineProperty(exports, "__esModule", { value: true });
 // Explain & TIP: When creating a Date object for a future event like a birthday, you might need to adjust the year based on whether the birthday has already occurred this year or not.
 function birthdayteller(month, date) {
-    const today = new Date();
-    let year = today.getFullYear();
-    let birthday = new Date(year, month - 1, date);
+    var today = new Date();
+    var year = today.getFullYear();
+    var birthday = new Date(year, month - 1, date);
     if (birthday < today) {
         birthday.setFullYear(year + 1);
     }
-    let diff = birthday.getTime() - today.getTime();
-    let daysleft = Math.ceil(diff / (1000 * 60 * 60 * 24));
-    return `Next birthday on ${birthday.toLocaleDateString()}
-Days left in your birthday:${daysleft}`;
+    var diff = birthday.getTime() - today.getTime();
+    var daysleft = Math.ceil(diff / (1000 * 60 * 60 * 24));
+    return "Next birthday on ".concat(birthday.toLocaleDateString(), "\nDays left in your birthday:").concat(daysleft);
 }
 console.log(birthdayteller(4, 5));
